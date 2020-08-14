98528417391cb6662bf141f6eae73211d35bb05a
Tom <tom@pop-os.localdomain>
2020-08-13 20:38:07 -0500

    added basic styles and divs to display hard code of progress bar

--- src/components/QuotesBar.vue
+++ src/components/QuotesBar.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Quote Bar</div>
+  <div>
+    <div class="header">
+      Quotes Added
+    </div>
+    <div class="outter-box">
+      <div class="inner-box">
+        2/10
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +18,24 @@
 </script>
 
 <style scoped>
+  .header {
+    font-size: 45px;
+  }
+  .outter-box {
+    width: 80%;
+    height: 20px;
+    border: 1px solid #333;
+    border-radius: 10px;
+    background-color: #aaa;
+    text-align: center;
+  }
+  .inner-box {
+    width: 100%;
+    height: 20px;
+    border-radius: 10px;
+    background-color: blue;
+    color: white;
+    
+  }
 
 </style>
