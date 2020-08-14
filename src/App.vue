7bf2b023932d0471c192d70e91d3baec88c4e953
Tom <tom@pop-os.localdomain>
2020-08-13 22:48:32 -0500

    moved quotes component back, pass array of quotes with styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <QuotesBar></QuotesBar>
-    <QuoteInputText></QuoteInputText>
+    <QuoteInputText @singleQuoteToPush="quotes.push($event)"></QuoteInputText>
+    <Quotes>
+      <div class="frow">
+        <div v-for="quote in quotes" :key="quote.length" class="row-between shadow-light p-10 m-10">
+            <p>{{ quote }}</p>
+        </div>
+      </div>
+    </Quotes>
     <Footer></Footer>
   </div>
 </template>
@@ -9,12 +16,19 @@
 <script>
 import QuotesBar from './components/QuotesBar';
 import QuoteInputText from './components/QuoteInputText';
+import Quotes from './components/Quotes'
 import Footer from './components/Footer';
 
   export default {
+    data() {
+      return {
+        quotes: [],
+      }
+    },
     components: {
       QuotesBar,
       QuoteInputText,
+      Quotes,
       Footer
     }
   }
