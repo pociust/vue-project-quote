3fd28c494ec43b0278aa1e1852572c14bbce5270
Tom <tom@pop-os.localdomain>
2020-08-13 22:58:26 -0500

    added footer with style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Footer</div>
+  <div class="frow items-center">Info: Click on a Quote to delete it</div>
 </template>
 
 <script>
@@ -9,5 +9,11 @@
 </script>
 
 <style scoped>
+  div {
+    height: 50px;
+    width: 80%;
+    background-color: lightsteelblue;
+    margin: auto;
+  }
 
 </style>
