dfb4c82e42017ae413e76acacc27c9adfd20ac0c
Tom <tom@pop-os.localdomain>
2020-08-13 21:12:41 -0500

    added bascic template content for input including styling and button with console log attachment

--- src/components/QuoteInputText.vue
+++ src/components/QuoteInputText.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>Quote Input Text</div>
+  <div class="container frow">
+    <div>
+      <div class="py-5">Quote</div>
+      <input type="text">
+      <button @click="addQuote">Add Quote</button>
+
+      </div>
+  </div>
 </template>
 
 <script>
   export default {
+    methods: {
+      addQuote() {
+        console.log('added')
+      }
+    }
   
   }
 </script>
 
 <style scoped>
-
+  .container {
+    padding: 50px;
+  }
+  input {
+    height: 75px;
+    width: 300px;
+    border: 1px solid #333;
+    border-radius: 5px;
+  }
+  button {
+    margin: 10px auto;
+  }
 </style>
